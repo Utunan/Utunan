d9e2fdb1c63667b1fc01a69081a50a1402c44437
xiaozhengwei <yan934934@outlook.com>
2018-10-23 11:15:20 +0800

    xiaozhengwei
    2018/10/23 update

---
+++ 前端界面/Utunan/js/header.js
@@ -0,0 +1,77 @@
+var searchtext = document.getElementById("searchtext");
+
+var bnav = document.getElementById('bnav')
+var bnavli=bnav.getElementsByTagName("li")
+
+var navbutton = document.getElementById('navbutton')
+
+var bnavtimer = false;
+var timer;
+var clicktimer = 0;
+
+searchtext.onmouseover=function(){
+    searchtext.style.backgroundColor='#fff'
+}
+searchtext.onfocus = function () {
+    searchtext.style.backgroundColor='#fff'
+    searchtext.style.color='#000'
+    if (searchtext.value == '搜索') {
+        searchtext.value = '';
+    }
+}
+
+searchtext.onblur = function () {
+    if (searchtext.value == "") {
+        searchtext.value = '搜索'
+    }
+}
+navbutton.onclick = function () {
+    bnav.style.display = 'block'
+    for(var i=0;i<4;i++){
+        bnavli[i].style.display='block'
+    }
+    bnav.style.height = bnav.offsetHeight
+    if (clicktimer % 2 == 0) {
+        clicktimer++;
+        if ((parseInt(bnav.style.height) == 0)) {
+            bnav.style.height = 0
+            clicktimer++
+            clearInterval(timer)
+            timer = setInterval(function () {
+                bnav.style.height = parseInt(bnav.style.height) + 5 + 'px';
+                if (bnav.style.height == '200px') {
+                    clearInterval(timer);
+                }
+            }, 1)
+        }
+    }
+    if (clicktimer % 2 == 1) {
+        clicktimer--;
+        clearInterval(timer)
+        timer = setInterval(function () {
+            bnav.style.height = parseInt(bnav.style.height) -5 + 'px';
+            if (bnav.style.height == '0px') {
+                clearInterval(timer);
+            }
+        }, 1)
+
+    }
+}
+
+window.onresize = function () {
+    if (window.innerWidth >= 959) {
+        bnav.style.height = 0;
+        for(var i=0;i<bnavli.length;i++){
+            bnavli[i].style.display=""
+        } 
+    }
+}
+
+var userbox =document.getElementById("userbox");
+userbox.onmouseover=function(){
+    userinfo.style.display="block";
+}
+
+userbox.onmouseout=function(){
+    userinfo.style.display="none";
+}
