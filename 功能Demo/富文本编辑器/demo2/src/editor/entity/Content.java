881273e42a989ee02c6ffd0df54a320c7175279a
闫正伟 <yan934934@outlook.com>
2018-11-03 12:40:22 +0800

    Revert "Revert "update 整理库""
    This reverts commit 6125d1762665096c3f60e519faa504bebce33dad.

---
+++ 功能Demo/富文本编辑器/demo2/src/editor/entity/Content.java
@@ -0,0 +1,22 @@
+package editor.entity;
+
+public class Content {
+    private int cid;
+    private String n;
+
+    public int getCid() {
+        return cid;
+    }
+
+    public void setCid(int cid) {
+        this.cid = cid;
+    }
+
+    public String getN() {
+        return n;
+    }
+
+    public void setN(String n) {
+        this.n = n;
+    }
+}
