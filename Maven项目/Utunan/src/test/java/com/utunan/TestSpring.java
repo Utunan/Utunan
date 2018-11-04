4b7558ce95e55f29e7f7dea97f79cc58f418a769
闫正伟 <yan934934@outlook.com>
2018-11-04 19:03:49 +0800

    2018/11/04 19:03 update

--- Maven项目/Utunan/src/test/java/com/utunan/TestSpring.java
+++ Maven项目/Utunan/src/test/java/com/utunan/TestSpring.java
@@ -1,4 +1,17 @@
 package com.utunan;
 
+import com.utunan.pojo.User;
+import javafx.application.Application;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class TestSpring {
+
+    @Test
+    public void s1(){
+        ApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
+        User girl=(User) context.getBean("user");
+        System.out.println(girl);
+    }
 }
