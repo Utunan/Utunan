31f0ea4133fee97b9a5d20d11b1adad4af1efbf3
闫正伟 <yan934934@outlook.com>
2018-11-04 18:49:28 +0800

    2018/11/04 18:49 update

--- Maven项目/Utunan/src/test/java/com/utunan/TestMybatis.java
+++ Maven项目/Utunan/src/test/java/com/utunan/TestMybatis.java
@@ -13,7 +13,7 @@ import java.util.Date;
 import java.util.List;
 
 
-public class TestClass {
+public class TestMybatis {
 
     @Test
     public void m1() {
---
+++ Maven项目/Utunan/src/test/java/com/utunan/TestSpring.java
@@ -0,0 +1,4 @@
+package com.utunan;
+
+public class TestSpring {
+}
