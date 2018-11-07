dc17dc6f7ef00232b0320079595acb09ef5a6d43
SunChengC1024 <1685975437@qq.com>
2018-11-07 15:57:36 +0800

    2018/11/7 15:57 update

--- Maven项目/Utunan/src/main/java/com/utunan/service/PostCollectorService.java
+++
@@ -1,8 +0,0 @@
-package com.utunan.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface PostCollectorService {
-
-}
--- Maven项目/Utunan/src/main/java/com/utunan/service/school/SchoolTypeService.java
+++
@@ -1,7 +0,0 @@
-package com.utunan.service.school;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface SchoolTypeService {
-}
