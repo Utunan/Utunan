1bb86c918f8ed38b975c9ea28e4f8f8b68ad21d6
闫正伟 <yan934934@outlook.com>
2018-11-17 00:02:30 +0800

    2018/11/17 00:02 update

--- Maven项目/Utunan/src/main/webapp/js/login.js
+++ Maven项目/Utunan/src/main/webapp/js/login.js
@@ -1,9 +1,7 @@
 ﻿permit = document.getElementById('permit');
 password = document.getElementById('password');
 reply = document.getElementById('reply');
-
-state = false;
-
+permit_state=true;
 function checkpermit() {
     var regt = /^0?1[3|4|5|6|7|8][0-9]\d{8}$/;
     var rege = new RegExp("^[a-z0-9]+([._\\-]*[a-z0-9])*@([a-z0-9]+[-a-z0-9]*[a-z0-9]+.){1,63}[a-z0-9]+$");
@@ -11,11 +9,16 @@ function checkpermit() {
 }
 
 permit.onblur = function () {
+
     if (permit.value == '') {
         permit.parentNode.style.border = '1px solid red';
+        permit.focus();
         reply.innerHTML = '通行证或密码不能为空'
+    }else if(checkpermit(permit.value)){
+        reply.innerHTML='通行证格式错误'
     } else {
-        permit_state = false;
+
+        permit_state=false;
         reply.innerHTML = ''
         var xmlhttp;
         if (window.XMLHttpRequest) {
@@ -23,19 +26,21 @@ permit.onblur = function () {
         } else {
             xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
         }
+
         //向后台发送验证信息
         xmlhttp.open("POST", "/checkpermit?permit=" + permit.value, true);
         xmlhttp.send();
+
         //对返回信息进行验证
         xmlhttp.onreadystatechange = function () {
             if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
                 var res = xmlhttp.responseText;
                 if (res == "unsuccessful") {
-                    reply.innerHTML = '通行证不存在,请先<a href="register">注册</a>';
+                    reply.innerHTML = '通行证不存在';
                     permit.parentNode.style.border = '1px solid red';
-                } else {
-                    reply.innerHTML = "";
-                    state = true;
+                }else{
+                    reply.innerHTML="";
+                    permit_state=true;
                     permit.parentNode.style.border = '1px solid LightSteelBlue';
                 }
             }
@@ -47,17 +52,25 @@ password.onblur = function () {
 
     if (password.value == '') {
         password.parentNode.style.border = '1px solid red';
-        if (state)
-            reply.innerHTML = '通行证或密码不能为空';
-        if (permit.value == '') {
-            permit.parentNode.style.border = '1px solid red';
+        reply.innerHTML = '通行证或密码不能为空';
+    } else {
+        if (password.value.length < 8 || password.value.length > 16) {
+            reply.innerHTML = '通行证或密码格式错误';
+            password.parentNode.style.border = '1px solid red';
+        } else {
+            reply.innerHTML = '';
+            password.parentNode.style.border = '1px solid LightSteelBlue';
         }
     }
 }
 
 
-function checkForm() {
 
+function checkForm() {
+    if(!permit_state) {
+        reply.innerHTML = '通行证或密码格式错误';
+        return false;
+    }
     if (password.value == '' || permit.value == '') {
         reply.innerHTML = '通行证或密码不能为空';
         if (password.value == '') {
@@ -66,12 +79,18 @@ function checkForm() {
         if (permit.value == '') {
             permit.parentNode.style.border = '1px solid red';
         }
-        state = false;
+        return false;
     }
 
     if (checkpermit()) {
         reply.innerHTML = '请使用手机号或邮箱登录'
-        state = false;
+        return false;
     }
-    return state;
+
+    if (password.value.length < 8 || password.value.length > 16) {
+         reply.innerHTML = '通行证或密码格式错误';
+        password.parentNode.style.border = '1px solid red';
+         return false;
+    }
+    return true;
 }
--- Maven项目/Utunan/target/Utunan/js/register.js
+++ Maven项目/Utunan/target/Utunan/js/register.js
@@ -6,32 +6,31 @@ rpassword = document.getElementById("rpassword");
 
 var gctimer = null;
 var issend = false;
-var state = true;
-var code_state = false;
-var reply_value = "";
-var check_state = true;
-var isregister = true;
+var gitimer;
+var ispermit = false;
+var state=true;
 
-function checkvcode(value) {
-    reg = /^\d{6}$/;
-    if (reg.test(value))
-        return true;
-    return false;
-}
 
-function checkpassword(value) {
-    var regu = "^[0-9a-zA-Z]{8,16}$";
-    var re = new RegExp(regu);
-    if (re.test(value)) {
-        return true;
-    } else {
-        return false;
-    }
-}
 
 function checkpermit(telephone) {
     var re = /^0?1[3|4|5|6|7|8][0-9]\d{8}$/;
     return re.test(telephone);
+}
+
+function CheckForm(){
+
+}
+
+
+userTelephone.onblur = function () {
+    if (!checkpermit(userTelephone.value)&&(issend==false)) {
+        userTelephone.parentNode.style.border = '1px solid red';
+        reply.innerHTML = '手机号填写错误'
+        userTelephone.focus();
+    } else {
+        userTelephone.parentNode.style.border = '1px solid LightSteelBlue';
+        reply.innerHTML = ''
+    }
 }
 
 getcode.onclick = function () {
@@ -42,102 +41,28 @@ getcode.onclick = function () {
         } else {
             xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
         }
-        //向后台发送验证信息
-        xmlhttp.open("POST", "/checkpermit?permit=" + userTelephone.value, true);
-        xmlhttp.send();
+        //Ajax的操作
 
-
-        //对返回信息进行验证
-        xmlhttp.onreadystatechange = function () {
-            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                var res = xmlhttp.responseText;
-                if (res == "successful") {
-                    reply.innerHTML = '手机号已被注册';
-                    userTelephone.parentNode.style.border = '1px solid red';
+        if (issend == false) {
+            issend = true;
+            gitimer = 60;
+            getcode.innerHTML = '60s后重新发送';
+            gctimer = setInterval(function () {
+                getcode.innerHTML = gitimer + 's后重新发送';
+                if (gitimer > 0) {
+                    gitimer--;
                 } else {
-                    reply.innerHTML = "";
-                    state = true;
-                    isregister = false;
-                    userTelephone.parentNode.style.border = '1px solid LightSteelBlue';
-                }
-            }
-        }
-        setTimeout(function () {
-                if (issend == false && !isregister) {
-                    issend = true;
-                    gitimer = 59;
-                    getcode.innerHTML = '60s后重新发送';
-                    //Ajax的操作
-                    xmlhttp.open("POST", "/code?userTelephone=" + userTelephone.value, true);
-                    xmlhttp.send();
-                    //对返回信息进行验证
-                    xmlhttp.onreadystatechange = function () {
-                        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                            var res = xmlhttp.responseText;
-                            code_state = true;
-                        }
-                    }
-                    gctimer = setInterval(function () {
-                        getcode.innerHTML = gitimer + 's后重新发送';
-                        if (gitimer > 0) {
-                            gitimer--;
-                        } else {
-                            clearInterval(gctimer);
-                            getcode.innerHTML = "获取验证码"
-                            issend = false
-                        }
-                    }, 1000)
-                } else {
-                    return false;
+                    clearInterval(gctimer);
+                    getcode.innerHTML = "获取验证码"
+                    issend = false
                 }
             }, 1000)
+        } else {
+            return false;
+        }
     } else {
         if (issend == false)
-            reply.innerHTML = '请输入正确的手机号码';
+            reply.innerHTML = '手机号格式错误';
     }
-}
-
-function checkForm() {
-    reply_value = ""
-    check_state = true;
-    if (!code_state) {
-        reply_value = "请输入验证码";
-        vcode.parentNode.style.border = '1px solid red';
-        check_state = false;
-    } else
-        vcode.parentNode.style.border = '1px solid LightSteelBlue';
-
-    if (!checkvcode(vcode.value)) {
-        reply_value = '请输入验证码';
-        vcode.parentNode.style.border = '1px solid red';
-        check_state = false;
-    }
-
-    if (!checkpermit(userTelephone.value)) {
-        userTelephone.parentNode.style.border = '1px solid red';
-        reply_value = '请输入正确的手机号码';
-        check_state = false;
-    } else {
-        userTelephone.parentNode.style.border = '1px solid LightSteelBlue';
-    }
-
-    if (password.value != rpassword.value) {
-        reply_value = '两次输入的密码不相同';
-        rpassword.parentNode.style.border = '1px solid red';
-        check_state = false;
-    } else {
-        rpassword.parentNode.style.border = '1px solid LightSteelBlue';
-    }
-
-    if(!checkpassword(password.value)){
-        reply_value = '密码格式不正确';
-        password.parentNode.style.border = '1px solid red';
-        check_state = false;
-    }else{
-        password.parentNode.style.border = '1px solid LightSteelBlue';
-    }
-    reply.innerHTML = reply_value;
-    console.log(check_state)
-    return check_state;
 }
 
