f240665163fb3ffa0f56e20c853e732f82646b64
SunChengC1024 <1685975437@qq.com>
2018-11-28 10:03:19 +0800

    2018/11/28 10:03

---
+++ Maven项目/Utunan/src/main/java/com/utunan/service/questionbank/impl/NoteServiceImpl.java
@@ -0,0 +1,9 @@
+package com.utunan.service.questionbank.impl;
+
+import com.utunan.service.questionbank.ChapterService;
+import org.springframework.stereotype.Service;
+
+@Service("chapterService")
+public class NoteServiceImpl implements ChapterService {
+
+}
