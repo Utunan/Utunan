0b5c2669b2798bd863db608fd16110a87c3b9c91
zhangzhengyang1998 <1810349233@qq.com>
2018-12-19 10:30:14 +0800

    2018/12/19   10:29

--- Maven项目/Utunan/src/main/java/com/utunan/mapper/admin/QADelMapper.java
+++ Maven项目/Utunan/src/main/java/com/utunan/mapper/admin/QADelMapper.java
@@ -8,4 +8,10 @@ public interface QADelMapper {
 
     //批量删除回答评论
     void delAllAnswer(@Param("b") Long[] b);
+
+    //单个删除提问
+    void delQuiz(@Param("quizId") Long quizId);
+
+    //单个删除回答评论
+    void delAnswer(@Param("answerId") Long answerId);
 }
--- Maven项目/Utunan/src/main/java/com/utunan/service/admin/AdminDelService.java
+++ Maven项目/Utunan/src/main/java/com/utunan/service/admin/AdminDelService.java
@@ -10,4 +10,10 @@ public interface AdminDelService {
 
     //批量删除院系评论
     void delAllSchoolComment(Long[] b);
+
+    //单个删除用户
+    void delMember(Long userId);
+
+    //删除单个学校评论
+    void delSchoolComment(Long schoolcommentId);
 }
