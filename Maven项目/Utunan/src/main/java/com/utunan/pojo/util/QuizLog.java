1b354bb01c76b19a4896859df4b4ee60e6a3f500
tangxi8823 <zxcvbnm8823@qq.com>
2019-05-28 16:47:51 +0800

    fix

--- Maven项目/Utunan/src/main/java/com/utunan/pojo/util/QuizLog.java
+++ Maven项目/Utunan/src/main/java/com/utunan/pojo/util/QuizLog.java
@@ -1,24 +1,26 @@
-package com.utunan.pojo.base.questionbank;
+package com.utunan.pojo.util;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import com.utunan.pojo.base.user.User;
 
-import java.util.Date;
+import java.text.SimpleDateFormat;
 
 /**
  * @author 王慧
  * @description: TODO
- * @date 2019/5/27 14:40
+ * @date 2019/5/28 10:37
  */
-public class QuestionLog {
-    private static Logger log = LoggerFactory.getLogger(QuestionLog.class);
+public class QuizLog {
+
+    private static Logger log = LoggerFactory.getLogger(QuizLog.class);
+    SimpleDateFormat df;
     private Long quizId;
     private Long userId;
     private String time;
+
     public Long getQuizId() {
         return quizId;
     }
-
     public void setQuizId(Long quizId) {
         this.quizId = quizId;
     }
@@ -26,17 +28,17 @@ public class QuestionLog {
     public Long getUserId() {
         return userId;
     }
-
     public void setUserId(Long userId) {
         this.userId = userId;
     }
+
     public String getTime() {
         return time;
     }
-
     public void setTime(String time) {
         this.time = time;
     }
+
     public void logsth() {
         log.info(userId+"  "+quizId+"  "+time);
     }
